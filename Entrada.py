6b563ac22bbe27d892aed7f4266a418e54331c7a
Miguel González Barajas <58783994+MikeGonzaBar@users.noreply.github.com>
2021-09-14 14:25:14 -0500

    Añadí las clases que usan la interfaz comida

---
+++ Entrada.py
@@ -0,0 +1,8 @@
+from Interfaces import Comida
+
+class Entrada(Comida):
+  def __str__(self):
+     return 'Clase entrada'
+
+  def servir():
+    return 'Sirviendo entrada'
---
+++ Postre.py
@@ -0,0 +1,8 @@
+from Interfaces import Comida
+
+class Postre(Comida):
+  def __str__(self):
+     return 'Clase postre'
+
+  def servir():
+    return 'Sirviendo postre'
